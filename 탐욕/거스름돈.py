f4cedd30f94863466aed3f17439c1edaddee7e3f
parkjungkwan <pakjkwan@gmail.com>
2022-10-07 15:39:02 +0900

    거스름돈

--- 탐욕/거스름돈.py
+++ 탐욕/거스름돈.py
@@ -17,38 +17,26 @@
          10원 : 2개
       *****************************
 '''
-import gc
-
 
 class Solution:
    def solution(self, money):
-      title = " ### 화폐교환 ###"
-      aster = "*"*30
-      answer = f"요청금액 : {money} 원"
       unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
-      # 이 자리에 화폐단위를 출력하는 코드를 넣으시오
+      dc = {}
       for i in unit:
-
-         print(i)
-
-      
-      '''
-      result = (
-         f'{title} \n {aster} \n {answer} \n {aster}\n '
-         f'5만원 : {answer2}매 \n '
-         f'1만원 : {answer3}매 \n '
-         f'5000원 : {answer4}매 \n '
-         f'1000원 : {answer5}매 \n '
-         f'500원 : {answer6}매 \n' 
-         f'100원 : {answer7}매 \n '
-         f'50원 : {answer8}매 \n '
-         f'10원 : {answer9}매'
-      )
-      '''
+         cnt = money // i
+         dc[i] = cnt
+         money = money % i
+      print('### 화폐교환 ###')
+      print("*"*30)
+      print(f"요청금액 : {money} 원")
+      for k, v in dc.items():
+         print(f'{k}원 : {v}매')
+      print("*"*30)
       return
    
 if __name__=="__main__":
    solution = Solution()
    money = int(input("화폐교환할 금액입력: "))
    print(solution.solution(money))
+   
  
